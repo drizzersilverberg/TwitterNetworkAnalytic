d111449b7b6b14b6bb7023227a444ae93839b63a
Dendi Handian <dendihandian@gmail.com>
2017-01-01 08:25:58 +0700

    remove api files

--- config.py
+++
@@ -1,7 +0,0 @@
-port = 5000
-api_port = 4000
-
-# consumer_key = 'Fill with your consumer_key'
-# consumer_secret = 'Fill with your consumer_secret'
-# access_token = 'Fill with your access_token'
-# access_token_secret = 'Fill with your access_token_secret'
--- networkxModule.py
+++
@@ -1,4 +0,0 @@
-from tweepyModule import userTweet
-from tweepyModule import topicTweet
-import networkx as net
-
